8affbecec0b219e059a42f1d57b49e4256c40c06
René Kooi <renee@kooi.me>
2016-09-02 19:51:40 +0200

    youtube: include https://www in youtube URL regex

--- src/embedders.js
+++ src/embedders.js
@@ -1,6 +1,4 @@
-import youTubeRegex from 'youtube-regex';
-
 export const generic = /\b(https?):\/\/\S+?\/\S+\.(?:jpe?g|gif|png|webp)\b(?:\?\S+)?/gi;
 export const webm = /\b(https?):\/\/\S+?\/\S+\.webm\b(?:\?\S+)?/gi;
 export const gifv = /\bhttps?:\/\/i\.imgur\.com\/(?:.{7})\.(?:gifv|mp4|webm)/gi;
-export const youTube = youTubeRegex();
+export const youTube = /\bhttps?:\/\/(?:www\.)?(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\/?\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})/gi;
